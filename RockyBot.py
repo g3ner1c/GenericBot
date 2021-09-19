bf986334e187f7d372cf9f227c7ef6b96c71158b
Banshee-72 <84111149+Banshee-72@users.noreply.github.com>
2021-09-19 13:43:52 -0500

    Update RockyBot.py

--- RockyBot.py
+++ RockyBot.py
@@ -1,17 +1,3 @@
-from flask import Flask
-from threading import Thread
-app = Flask('')
-@app.route('/')
-def main(): 
-    return "Running"
-
-def run():
-    app.run(host="0.0.0.0", port=8000)
-
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
-
 import asyncio
 import math
 import os
