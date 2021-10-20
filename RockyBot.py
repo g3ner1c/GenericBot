4aa5110fc2f8dbcf143d3b754f052ca32fb71a76
Rocky <74469257+awesomeplaya211@users.noreply.github.com>
2021-10-20 14:21:49 +0000

    patched

--- RockyBot.py
+++ RockyBot.py
@@ -661,12 +661,12 @@ async def flag(ctx):
 
     await ctx.send(vp.flag_src(country))
 
-    def is_correct(_ctx):
-        return _ctx.author == ctx.author
+    # def is_correct(_ctx):
+    #     return _ctx.author == ctx.author
     
     while True:
 
-        action = await bot.wait_for('message',  check=is_correct)
+        action = await bot.wait_for('message',  check=True)
 
         if any(i.casefold() == action.content.casefold() for i in vp.iso()[country]):
 
